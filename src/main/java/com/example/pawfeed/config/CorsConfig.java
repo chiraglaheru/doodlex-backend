effc97afb9bd5f0c07090e278286c5c5dbf6784e
Chirag <140259132+chiraglaheru@users.noreply.github.com>
2026-04-15 20:20:41 +0530

    Update CorsConfig.java

--- src/main/java/com/example/pawfeed/config/CorsConfig.java
+++ src/main/java/com/example/pawfeed/config/CorsConfig.java
@@ -1,25 +1,17 @@
 package com.example.pawfeed.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(
-                            "http://localhost:3000",
-                            "https://doodle-x-7uxz.vercel.app"  
-                        )
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
