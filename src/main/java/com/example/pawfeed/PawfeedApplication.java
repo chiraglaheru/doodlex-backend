f9ddf8fba534b1ea4e9f30e5cb737cb0dd62c5fb
Chirag <140259132+chiraglaheru@users.noreply.github.com>
2026-04-15 22:57:33 +0530

    Update PawfeedApplication.java

--- src/main/java/com/example/pawfeed/PawfeedApplication.java
+++ src/main/java/com/example/pawfeed/PawfeedApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.example.pawfeed")
+@EnableScheduling
 public class PawfeedApplication {
 
 	public static void main(String[] args) {
