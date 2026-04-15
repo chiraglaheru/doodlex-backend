ccf6645fd17877c77a964a2eaf6f00a9b23cc12a
Chirag <140259132+chiraglaheru@users.noreply.github.com>
2026-04-15 20:02:24 +0530

    Update CorsConfig.java

--- src/main/java/com/example/pawfeed/config/CorsConfig.java
+++ src/main/java/com/example/pawfeed/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000","https://doodle-x-7uxz.vercel.app/")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
